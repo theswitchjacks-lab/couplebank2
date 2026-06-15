b7cc53225e5e990fab6425bf81122d4cfa39c0ff
Takeru <theswitchjacks@gmail.com>
2026-06-15 21:22:54 +0900

    fix port

--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def api_save_record():
 # Flask 起動
 # -----------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
